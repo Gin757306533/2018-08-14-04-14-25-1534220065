d4f499ee1ce14f4ecbec6a41fd1650c11cf5409b
Gin757306533 <757306533@qq.com>
2019-05-30 22:01:30 +0800

    my answer

--- main/main.js
+++ main/main.js
@@ -1,4 +1,29 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+"use strict";
+module.exports = function main(nums) {
+    var array_s = [
+     ["._.", "|.|", "|_|"],
+     ["...", "..|", "..|"],
+     ["._.", "._|", "|_."],
+     ["._.", "._|", "._|"],
+     ["...", "|_|", "..|"],
+     ["._.", "|_.", "._|"],
+     ["._.", "|_.", "|_|"],
+     ["._.", "..|", "..|"],
+     ["._.", "|_|", "|_|"],
+     ["._.", "|_|", "..|"]
+
+    ];
+    let ret_s = "";
+    for (var i = 0; i < 3;i++)
+    {
+    	for(var j = 0; j < nums.length; j++){
+    	    var n = parseInt(nums[j]);
+    	    ret_s += array_s[n][i];
+    	    if(j != nums.length - 1){
+    	    	ret_s += ' ';
+    	    }
+        }
+        ret_s +='\n';
+    }
+    return ret_s;
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,3 +1,4 @@
+"use strict";
 const main = require('../main/main');
 
 describe('main()', () => {
